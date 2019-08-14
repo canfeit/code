187e56e8128db7627085060433ed333e7163ca8f
canfeit <canfeit@qq.com>
2019-08-14 15:22:36 +0800

    add python类型和变量

--- pages/basic/types.js
+++ pages/basic/types.js
@@ -33,6 +33,27 @@ let strLength = (someValue as string).length;`}
         </SyntaxHighlighter>
         <> 1</>
         <> 2</>
-        <> 3</>
+        <SyntaxHighlighter language="python">
+            {`#整数
+-80#十进制整数
+0xff00#十六进制整数
+#浮点数
+1.23#小数
+1.2e-5#科学计数法表示0.000012
+#字符串，单引号'或双引号"括起来的任意文本
+"abc"#字符串abc
+'I\'m OK!'#I'm OK!，\是转移字符
+r'\n表示换行'#\n表示换行，r'表示不转义
+'''三个单引号或三个双引号
+可以表示多行文本
+可用于多行注释'''#'''表示多行文本,r'''表示不转义多行文本
+#布尔值，只有True、False两种值
+3 > 2#True,布尔运算
+5 > 3 and 3 > 1#True,与运算，只有所有都为True，结果才是True
+5 > 3 or 1 > 3#True,或运算，只要其中有一个为True，结果就是True
+not 1 > 2 #True,非运算，把True变成False，False变成True
+#空值用None表示
+`}
+        </SyntaxHighlighter>
     </Layout>
 }
--- pages/index.js
+++ pages/index.js
@@ -37,11 +37,12 @@ fn main() {
   }
 }`}
       </SyntaxHighlighter></>
-    <>
+    <> Python使用缩进来组织代码块
       <SyntaxHighlighter language="python">
         {`#!/usr/bin/python
 # -*- coding: UTF-8 -*-
- 
+# 以#开头的语句是注释，注释是给人看的，可以是任意内容，解释器会忽略掉注释。
+# 除注释外，每一行都是一个语句，当语句以冒号:结尾时，缩进的语句视为代码块 
 print "你好，世界";`}
       </SyntaxHighlighter></>
   </Layout>
