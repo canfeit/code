7745ef0b3c48e40bc19c5b8c9fb7cd5487c8894e
canfeit <canfeit@qq.com>
2020-05-19 13:56:38 +0800

    fix

--- pages/basic/asi.js
+++ pages/basic/asi.js
@@ -1,9 +1,10 @@
-import Layout from '../layout';
-import { Prism as Code } from 'react-syntax-highlighter';
+import Layout from "../../layout";
+import { Prism as Code } from "react-syntax-highlighter";
 export default () => {
-    return <Layout cat='basic' id='asi'>
-        <Code language="typescript">
-            {`
+  return (
+    <Layout cat="basic" id="asi">
+      <Code language="typescript">
+        {`
 //多行优先，解析器会尽量将新行并入当前行，当且仅当符合ASI规则时才会将新行视为独立的语句。
 /*ASI规则
 continue,return,break,throw后自动插入分号
@@ -15,18 +16,18 @@ continue,return,break,throw后自动插入分号
 1 + 2 +
 3//6
 `}
-        </Code>
-        <Code language="rust">
-            {`// 这是注释内容，将会被编译器忽略掉
+      </Code>
+      <Code language="rust">
+        {`// 这是注释内容，将会被编译器忽略掉
 // 这是主函数
 fn main() {
     // 调用编译生成的可执行文件时，这里的语句将被运行。
     // println! 是一个宏（macros），可以将文本输出到控制台（console）
     println!("Hello World!");
 }`}
-        </Code>
-        <Code language="kotlin">
-            {`/*
+      </Code>
+      <Code language="kotlin">
+        {`/*
 */
 //code hello.kt
 fun main(args: Array<String>) {//入口，该函数有一个名为 args、类型为“字符串数组”的参数。args 会包含调用程序的命令行参数，类似于 Python 中的 sys.argv；如果程序并不需要接受命令行参数并且使用的是 Kotlin 1.3，那么可以省略该参数，函数体由花括号分隔——缩进在 Kotlin 中通常不重要1，但是为了肉眼可读性理应正确缩进代码。
@@ -40,15 +41,19 @@ fun main(args: Array<String>) {//入口，该函数有一个名为 args、类型
 1 + 2 +
 3//6
 `}
-        </Code>
-        <> Python使用缩进来组织代码块
-      <Code language="python">
-                {`#!/usr/bin/env python3
+      </Code>
+      <>
+        {" "}
+        Python使用缩进来组织代码块
+        <Code language="python">
+          {`#!/usr/bin/env python3
 #不存在自动合并
 1 + 2#3
 + 3#3
 1 + 2 +#错误
 3`}
-            </Code></>
-    </Layout >
-}
+        </Code>
+      </>
+    </Layout>
+  );
+};
